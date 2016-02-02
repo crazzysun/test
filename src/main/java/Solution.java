be9cacb1e167c028d0ec1c7862b1ece274a2d061
student <student@c571.carina.griddynamics.net>
2016-02-02 14:51:42 +0300

    Initial commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,67 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import org.bson.Document;
+import com.mongodb.Block;
+import com.mongodb.client.FindIterable;
+
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Sorts.ascending;
+import static java.util.Arrays.asList;
+
+import static java.util.Arrays.asList;
+
+public class Solution {
+    MongoDatabase db;
+
+    void run() throws ParseException {
+        MongoClient mongoClient = new MongoClient();
+        db = mongoClient.getDatabase("test");
+        makeDB();
+
+
+        FindIterable<Document> iterable = db.getCollection("restaurants").find();
+
+
+        System.out.println(iterable.first());
+
+
+
+
+    }
+
+    void makeDB() throws ParseException {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
+        db.getCollection("restaurants").insertOne(
+                new Document("address",
+                        new Document()
+                                .append("street", "2 Avenue")
+                                .append("zipcode", "10075")
+                                .append("building", "1480")
+                                .append("coord", asList(-73.9557413, 40.7720266)))
+                        .append("borough", "Manhattan")
+                        .append("cuisine", "Italian")
+                        .append("grades", asList(
+                                new Document()
+                                        .append("date", format.parse("2014-10-01T00:00:00Z"))
+                                        .append("grade", "A")
+                                        .append("score", 11),
+                                new Document()
+                                        .append("date", format.parse("2014-01-16T00:00:00Z"))
+                                        .append("grade", "B")
+                                        .append("score", 17)))
+                        .append("name", "Vella")
+                        .append("restaurant_id", "41704620"));
+
+    }
+
+    public static void main(String[] args) throws ParseException {
+        new Solution().run();
+    }
+}
